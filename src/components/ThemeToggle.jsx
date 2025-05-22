adc8c488dc95787d8cdcdd9a12a03bc462dfb389
HelloDuongNha <hoangzuong9a1306@gmail.com>
2025-05-22 11:57:15 +0700

    change theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,20 +1,23 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
-
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if (storedTheme === "dark") {
+
+        if (storedTheme === "light") {
+            setIsDarkMode(false);
+            document.documentElement.classList.remove("dark");
+        } else {
+            // Mặc định là dark nếu chưa từng lưu
             setIsDarkMode(true);
             document.documentElement.classList.add("dark");
-        } else {
-            localStorage.setItem("theme", "light");
-            setIsDarkMode(false);
+            localStorage.setItem("theme", "dark");
         }
     }, []);
+
     const toggleTheme = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove("dark");
